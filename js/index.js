f34f73a2f89a23033ea0c59e1e6936015484082c
qiang.lu1 <qiang.lu1@baozun.cn>
2017-04-26 13:16:56 +0800

    添加导航

--- js/index.js
+++ js/index.js
@@ -21,9 +21,10 @@
     ary.forEach((item,i)=>{
       window[item+'_native'] = ary[item];
     })
-    console.log('sdf')
     document.querySelector('#content').style.minHeight = window.innerHeight+'px';
-  var sidebarDom = document.querySelector('#sidebar');
+  var sidebarDom = document.querySelector('#sidebar'),
+      contentDom = document.querySelector('#detail'),
+      backTopDom = document.querySelector('#backTop');
   getData({url:'readme.md',
   		data:'',
   		key:'',
@@ -42,7 +43,6 @@
   				})
   			}
   		})
-  var contentDom = document.querySelector('#detail');
   function getPage(){
   	return location.search.split('=')[1] || 'symbol';
   }
@@ -77,6 +77,7 @@
         Prism.highlightElement(dom);
       });
       updateSrc(contentDom);
+      updateTitelId(contentDom);
   }
   // 修改contentdom下面img的路径
   function updateSrc(dom) {
@@ -86,19 +87,120 @@
       dom.setAttribute('src',src.substring(3));
     })
   }
+  function updateTitelId(dom){
+    var h2Doms = dom.querySelectorAll('h2'),html='<ul>';
 
-  function getData(arg){
-  	var key = arg.key,
-  		store = window.sessionStorage,
-  		data;
-  	// if(!!key && !!(data = store.getItem(key))){
-  	// 	arg.fn && arg.fn(data);
-  	// 	return;
-  	// }
-  	window.fetch(arg.url).then(data=>data.text()).then(data=>{arg.fn&&arg.fn(data),store.setItem(key,data)}).catch(err=>console.log(`error:${err}`));
+    h2Doms.length>0 && forEach_native.call(h2Doms,function(item,i){
+      item.id = 'title' + i;
+      html += '<li><a href="javaScript:;" data-href="#'+item.id+'">'+ item.innerText +'</a></li>';
+    })
+    html += '</ul>';
+    var div = document.createElement('div');
+    div.id = "titleNav";
+    div.innerHTML = html;
+    contentDom.querySelector('h1').insertAdjacentElement('afterend',div);
+    forEach_native.call(contentDom.querySelectorAll('#titleNav li a'),(navDom,index)=>{
+      navDom.addEventListener('click',function(e){
+        var self = this,
+            href = self.dataset.href;
+            moveToMiddle(href, 0, '', 'top')
+
+      })
+    })
   }
-
   window.onpopstate = function(event) {
   	updateContent();
   };
+  backTopDom.addEventListener('click',function(){
+    moveToMiddle('#titleNav',0);
+  })
+
+  function getData(arg){
+    var key = arg.key,
+    store = window.sessionStorage,
+    data;
+    // if(!!key && !!(data = store.getItem(key))){
+    // 	arg.fn && arg.fn(data);
+    // 	return;
+    // }
+    window.fetch(arg.url).then(data=>data.text()).then(data=>{arg.fn&&arg.fn(data),store.setItem(key,data)}).catch(err=>console.log(`error:${err}`));
+  }
+  // 将一个dom移动到页面中间或者上方
+  function moveToMiddle(elem, height, fn, pos) {
+    var pos = pos || 'middle',
+    winWidth = window.innerWidth,
+    winHeight = window.innerHeight,
+    height = parseInt(height) || 0,
+    btnCurOftTop = document.querySelector(elem).getBoundingClientRect().top,
+    headerHeight = 0,
+    posObj = {},
+    curScrollTop = document.documentElement.scrollTop,
+    diffTop;
+    posObj.middle = (winHeight - headerHeight - height) / 2 + headerHeight;
+    posObj.top = headerHeight;
+    if (height > winHeight - headerHeight) {
+      pos = 'top';
+    }
+    diffTop = btnCurOftTop - posObj[pos];
+    setTimeout(function() {
+      animate1(diffTop, fn);
+    }, 200)
+  }
+  var animationFrame =  (function(window){
+    return window.requestAnimationFrame ||    //IE10以及以上版本，以及最新谷歌，火狐版本
+    window.webkitRequestAnimationFrame ||   //谷歌老版本
+    window.mozRequestAnimationFrame ||   //火狐老版本
+    function(callback){    //IE9以及以下版本
+      window.setTimeout(callback , 1000/60);  //这里强制让动画一秒刷新60次，这里之所以设置为16.7毫秒刷新一次，是因为requestAnimationFrame默认也是16.7毫秒刷新一次。
+    }
+  })(window);
+  // 运动函数
+  function animate1(diff, fn, step) {
+    var diff = diff || 0,
+    step = step || 3,
+    tempStep = step,
+    num = 10,
+    flag = diff > 0 ? true : false,
+    $win = document.body,
+    curScrollTop,
+    count = 0,
+    lastScrollTop;
+    (function move() {
+      curScrollTop = $win.scrollTop;
+      if (curScrollTop === lastScrollTop) {
+        !!fn && fn();
+        return;
+      }
+      count++;
+      if(count > 1000){
+        return;
+        console.log('error');
+      };
+      lastScrollTop = curScrollTop;
+      if (flag) {
+        if (diff > num / 2) {
+          tempStep = -diff / num;
+          diff += tempStep;
+          animationFrame(move);
+        } else {
+          tempStep = diff;
+          diff -= tempStep;
+          !!fn && fn();
+        }
+
+      } else {
+        if (diff < -num / 2) {
+          tempStep = -diff / num;
+          diff += tempStep;
+          $win.scrollTop = curScrollTop - tempStep;
+          animationFrame(move);
+        } else {
+          tempStep = -diff;
+          diff += tempStep;
+          !!fn && fn();
+        }
+      }
+      $win.scrollTop = curScrollTop - tempStep;
+    })()
+  }
 })()
